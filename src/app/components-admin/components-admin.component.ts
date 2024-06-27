60613711b43de206e703cf5c20b4885c8fc40e0a
Yasser Taibi <ytaibi@sqli.com>
2024-06-27 04:54:39 +0200

    DRAF: interface dashboard

--- src/app/components-admin/components-admin.component.ts
+++ src/app/components-admin/components-admin.component.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, ViewEncapsulation} from '@angular/core';
 import {RouterOutlet} from "@angular/router";
+import {HeaderComponent} from "./header/header.component";
+import {SidebarComponent} from "./sidebar/sidebar.component";
+import {FooterComponent} from "./footer/footer.component";
 
 @Component({
   selector: 'app-components-admin',
   standalone: true,
-    imports: [
-        RouterOutlet
-    ],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    SidebarComponent,
+    FooterComponent
+  ],
   templateUrl: './components-admin.component.html',
-  styleUrl: './components-admin.component.css'
+  styleUrls: ['./components-admin.component.css', '../../assets/css/admin-styles.css'],
+  encapsulation: ViewEncapsulation.Emulated // Adjust as needed
 })
 export class ComponentsAdminComponent {
 
--- src/app/components-admin/components-admin.module.ts
+++ src/app/components-admin/components-admin.module.ts
@@ -1,17 +1,35 @@
-import { NgModule } from '@angular/core';
+import {NgModule, OnInit} from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
 import { ComponentsAdminRoutingModule } from './components-admin-routing.module';
 import { ComponentsAdminComponent  } from './components-admin.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { FooterComponent } from './footer/footer.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+
+  ],
   imports: [
     RouterModule,
     CommonModule,
     ComponentsAdminRoutingModule,
-    ComponentsAdminComponent
-  ]
+    ComponentsAdminComponent,
+    HeaderComponent,
+    SidebarComponent,
+    FooterComponent,
+    DashboardComponent
+  ],
 })
-export class ComponentsAdminModule { }
+export class ComponentsAdminModule implements OnInit {
+
+
+  ngOnInit(): void {
+
+  }
+
+
+}
