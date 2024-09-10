17c0968e62662d09332d326d2d67c835751d6799
Yasser Taibi <ytaibi@sqli.com>
2024-09-10 15:47:03 +0200

    create Modals

--- src/app/components-admin/menu/article-table/article-table.component.ts
+++ src/app/components-admin/menu/article-table/article-table.component.ts
@@ -1,10 +1,12 @@
-import {Component, Input} from '@angular/core';
+import {Component, inject, Input, TemplateRef} from '@angular/core';
 import {Menu} from "../../../models/menu";
 import {NgxPaginationModule} from "ngx-pagination";
 import {FormsModule} from "@angular/forms";
 import {NgIf, NgOptimizedImage} from "@angular/common";
 import {DeleteArticleModalComponent} from "../delete-article-modal/delete-article-modal.component";
 import {EditArticleModalComponent} from "../edit-article-modal/edit-article-modal.component";
+import { ModalDismissReasons, NgbDatepickerModule, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-article-table',
@@ -15,6 +17,7 @@ import {EditArticleModalComponent} from "../edit-article-modal/edit-article-moda
     NgOptimizedImage,
     DeleteArticleModalComponent,
     EditArticleModalComponent,
+    NgbDatepickerModule,
     NgIf
   ],
   templateUrl: './article-table.component.html',
@@ -25,31 +28,40 @@ export class ArticleTableComponent {
   @Input() tableTitle: string = '';
   @Input() tableImage: string = '';
 
-  selectedMenu: any = null;
-  isEditModalOpen: boolean = false;
-  isDeleteModalOpen: boolean = false;
-
   //Pagination
-   p: number = 1;
+  p: number = 1;
+  searchText: string = '';
+  selectedMenu: any = null;
+  // isEditModalOpen: boolean = false;
+  // isDeleteModalOpen: boolean = false;
 
-   searchText: string = '';
+  private modalService = inject(NgbModal);
 
-  openEditModal(menu: any) {
-    this.selectedMenu = menu;
-    this.isEditModalOpen = true;
+  openEditModal(templateEdit: TemplateRef<any>,menu: any) {
+    this.modalService.open(templateEdit, { ariaLabelledBy: 'modal-basic-title' }).result.then(
+      (result) => {
+        this.selectedMenu = menu;
+        // this.isEditModalOpen = true;
+      },
+      (reason) => {
+        console.log("test de fermeture de modal");
+      },
+    );
+
     }
 
   openDeleteModal(menu: any) {
+
     this.selectedMenu = menu;
-    this.isDeleteModalOpen = true;
+    // this.isDeleteModalOpen = true;
     }
 
   closeEditModal() {
-      this.isEditModalOpen = false;
+      // this.isEditModalOpen = false;
     }
 
     closeDeleteModal() {
-      this.isDeleteModalOpen = false;
+      // this.isDeleteModalOpen = false;
     }
 
    trackById(index: number, item: any): number {
