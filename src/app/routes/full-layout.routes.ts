5e285776c15942f608307f5f9b07b42a0068521b
Yasser Taibi <ytaibi@sqli.com>
2024-09-24 16:37:50 +0200

    update branch CF_005

--- src/app/routes/full-layout.routes.ts
+++ src/app/routes/full-layout.routes.ts
@@ -6,15 +6,11 @@ import {ReserverTableModule} from "../components-admin/reservation/reserver-tabl
 //import { RoleGuard } from '../shared/role.guard';
 
 export const FULL_ROUTES: Routes = [
-
   { title: "ARÔTERRA | Dashboard", path: 'dashboard', loadChildren: () => import('../components-admin/dashboard/dashboard.module').then(m => m.DashboardModule) },
   { title: "ARÔTERRA | Profile", path: 'profile', loadChildren: () => import('../components-admin/profile/profile.module').then(m => m.ProfileModule) },
   { title: "ARÔTERRA | Réservation", path: 'consulter-reservations', loadChildren: () => import('../components-admin/reservation/consulter-reservations/consulter-reservations.module').then(m => m.ConsulterReservationsModule) },
   { title: "ARÔTERRA | Réservation", path: 'reserver-table', loadChildren: () => import('../components-admin/reservation/reserver-table/reserver-table.module').then(m => m.ReserverTableModule) },
-<<<<<<< HEAD
   { title: "ARÔTERRA | Menu & Tarifs", path: 'add-articles', loadChildren: () => import('../components-admin/menu/add-articles/add-articles.module').then(m => m.AddArticlesModule) },
-  { title: "ARÔTERRA | Menu & Tarifs", path: 'consulter-menu', loadChildren: () => import('../components-admin/menu/consulter-menu/consulter-menu.module').then(m => m.ConsulterMenuModule) }
-=======
+  { title: "ARÔTERRA | Menu & Tarifs", path: 'consulter-menu', loadChildren: () => import('../components-admin/menu/consulter-menu/consulter-menu.module').then(m => m.ConsulterMenuModule) },
   { title: "ARÔTERRA | Réservation", path: 'calendrier-reservations', loadChildren: () => import('../components-admin/reservation/calendrier-reservations/calendrier-reservations.module').then(m => m.CalendrierReservationsModule) }
->>>>>>> CF_S005
 ]
